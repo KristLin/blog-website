3c64e107476e23267bc409c71c0741d890b47d0f
KristLin <kristlin.unsw@gmail.com>
2019-09-06 13:22:09 +1000

    Add compose function

--- app.js
+++ app.js
@@ -9,11 +9,38 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 var homeContent = "This is the content of the home page.";
+var aboutContent = "This is the content of the about page.";
+var contactContent = "This is the content of the contact page.";
+
+var posts = [];
 
 app.get("/", function(req, res) {
-  res.render("home", {homeContent: homeContent});
+  res.render("home", {
+    homeContent: homeContent,
+    posts: posts
+  });
 });
 
+app.get("/about", function(req, res) {
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+  const newPost = {
+    title: req.body.postTitle,
+    content: req.body.postContent
+  };
+  posts.push(newPost);
+  res.redirect("/");
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
