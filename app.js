0a34cf0a8b0d29d7898d8bb6d36091af8d852001
KristLin <kristlin.unsw@gmail.com>
2019-09-15 23:52:43 +1000

    1. Implement MongoDB using Mongoose 2. Add "Compose" button in navigation bar 3. Update home page's screenshot

--- app.js
+++ app.js
@@ -3,63 +3,90 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const baseURL = "mongodb://localhost:27017";
+mongoose.connect(baseURL + "/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// set post schema
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+// set post model
+const Post = mongoose.model("Post", postSchema);
+
+// default content for different pages
 var homeContent = "This is the content of the home page.";
 var aboutContent = "This is the content of the about page.";
 var contactContent = "This is the content of the contact page.";
 
-var posts = [];
-
+// get home page
 app.get("/", function(req, res) {
-  res.render("home", {
-    homeContent: homeContent,
-    posts: posts
+  Post.find({}, function(err, posts) {
+    if(err) {
+      console.log(err);
+    }
+    res.render("home", {
+      homeContent: homeContent,
+      posts: posts
+    });
   });
 });
 
+// get about page
 app.get("/about", function(req, res) {
   res.render("about", {aboutContent: aboutContent});
 });
 
+// get contact page
 app.get("/contact", function(req, res) {
   res.render("contact", {contactContent: contactContent});
 });
 
+// get compose page
 app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
+// handle compose post request
 app.post("/compose", function(req, res) {
-  const newPost = {
+  const newPost = new Post({
     title: req.body.postTitle,
     content: req.body.postContent
-  };
-  posts.push(newPost);
-  res.redirect("/");
+  });
+  // save the new post
+  newPost.save(function(err) {
+    if(err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
 });
 
-app.get("/posts/:postTitle", function(req, res) {
-  const postTitle = _.lowerCase(req.params.postTitle);
-  var find = false;
-  posts.forEach(function(post) {
-    const storedPostTitle = _.lowerCase(post.title);
-    if(storedPostTitle === postTitle) {
-      find = true;
+// get single post page
+app.get("/posts/:postId", function(req, res) {
+  const postId = req.params.postId;
+  Post.findOne({_id: postId}, function(err, post) {
+    if(err) {
+      console.log(err);
+    }
+    if(!post) {
+      res.render("404");
+    } else {
       res.render("post", {post: post});
     }
   });
-  if(!find) {
-    res.render("404");
-  }
-
 });
-
-
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
